5b3941c831ce31ccd9d650121033a4eccedbe7a6
Tushar <tushar8650@outlook.com>
2023-05-16 17:34:37 +0530

    added mongob, express, nodemon as well as fix nodemon errors

--- mynotebook-backend/db.js
+++ mynotebook-backend/db.js
@@ -1,10 +1,15 @@
-const mongoose = require('mongoose');
-const mongoURI = "";
+const mongoose = require("mongoose");
 
-const connectToMongo = ()=>{
-    mongoose.connect(mongoURI, ()=>{
-        console.log('connected')
-    })
-}
+const mongoURI = "mongodb://127.0.0.1:27017/?directConnection=true";
+// localhost to 127.0.0.1 because of an error
+
+const connectToMongo = async ()=>{
+    try {
+        mongoose.connect(mongoURI);
+        console.log("Connected to Mongo Successfully!");
+      } catch (error) {
+        console.log(error);
+      }
+    };
 
 module.exports = connectToMongo;
