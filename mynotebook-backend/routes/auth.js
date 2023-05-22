90f513047cec86badbc79aab06b10f7bf6376b80
Tushar <tushar8650@outlook.com>
2023-05-22 07:26:54 +0530

    added bcrpyt and jsonwebtoken

--- mynotebook-backend/routes/auth.js
+++ mynotebook-backend/routes/auth.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 const { body, validationResult } = require('express-validator'); //express-validator version 6.12.0
 const User = require('../models/User')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
+const JWT_SECRET = 'tushartushar@';
 
 // Create a user using: POST "/api/auth/createuser" , No login require
 // we will create an array in router.post for validations
@@ -22,13 +25,25 @@ router.post('/createuser',[
     if(user){
         return res.status(400).json({error: "sorry a user with this email already exists."})
     }
+    // for hashing, salting we are using bcrypt.js
+    const salt = await bcrypt.genSalt(10);
+    const secPass = await bcrypt.hash(req.body.password, salt)
+    // creating new user
         user = await User.create({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password,
+        password: secPass,
       })
 
-    res.json(user)
+    
+    const data = {
+        user: user.id
+    }  
+    const authtoken = jwt.sign(data, JWT_SECRET);  
+
+      res.json({authtoken})
+    // res.json(user) 
+
     }catch (error){
         console.error(error.message);
         res.status(500).send("some error occured");
